cbe1130fc7b343755aeb4f9d5f60411ac0a0ce51
yeongminkim <oinochoe@naver.com>
2020-10-17 18:11:04 +0900

    deno 서버. name 및 group name 없으면 chat.html에서 아이디 및 그룹 선택하게 하기

--- public/client.js
+++ public/client.js
@@ -3,13 +3,33 @@ let ws;
 window.addEventListener('DOMContentLoaded', () => {
     ws = new WebSocket(`ws://localhost:3000/ws`);
     ws.addEventListener('open', onConnectionOpen);
-    ws.addEventListener('open', onMessageReceived);
+    ws.addEventListener('message', onMessageReceived);
 });
 
 function onConnectionOpen() {
-    console.log('connection opened');
+    const queryParams = getQueryParams();
+    if (!queryParams.name || !queryParams.group) {
+        window.location.href = 'chat.html';
+    }
+    const event = {
+        event: 'join',
+        groupName: queryParams.group,
+        name: queryParams.name,
+    };
+    ws.send(event);
 }
 
 function onMessageReceived(message) {
     console.log(message);
+}
+
+function getQueryParams() {
+    const search = window.location.search.substring(1);
+    const pairs = search.split('&');
+    const params = {};
+    for (const pair of pairs) {
+        const parts = pair.split('=');
+        params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+    }
+    return params;
 }
